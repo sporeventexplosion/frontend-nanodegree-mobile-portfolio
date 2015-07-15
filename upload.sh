48ec767d5336b1aa7a9b51b5ec2612881dc7db85
Shroomy Shroom <sporeventexplosion@gmail.com>
2015-07-15 19:23:16 +0300

    testing 6

--- upload.sh
+++ upload.sh
@@ -13,14 +13,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis CI"
 
   #using token clone gh-pages branch
-  #git clone --branch=gh-pages --depth 1 https://${GH_TOKEN}@github.com/sporeventexplosion/frontend-nanodegree-mobile-portfolio  gh-pages > /dev/null
-  git clone --branch=gh-pages --depth 1 https://${GH_TOKEN}@github.com  gh-pages > /dev/null
+  git clone --branch=gh-pages --depth 1 https://${GH_TOKEN}@github.com/sporeventexplosion/frontend-nanodegree-mobile-portfolio  gh-pages > /dev/null
+
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
 
   echo "INCOMING DIRECTORY LIST":
-  ls
+  tree
   cp -R $HOME/build/* .
   echo "Copied"
 
