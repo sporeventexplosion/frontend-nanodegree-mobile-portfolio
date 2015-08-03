a1172eef3943303099d417019c9c8c90153226f8
Shroomy Shroom <sporeventexplosion@gmail.com>
2015-08-03 22:09:20 +0300

    again use existing randomPizzas variable

--- source/views/js/main.js
+++ source/views/js/main.js
@@ -456,8 +456,7 @@ window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
-  pizzasDiv.appendChild(pizzaElementGenerator(i));
+  randomPizzas.appendChild(pizzaElementGenerator(i));
 }
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
